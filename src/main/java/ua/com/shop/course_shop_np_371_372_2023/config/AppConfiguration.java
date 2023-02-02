8ffd1084a7abb0162874d3b7633e57931488a0cb
“Yurij <yuralm@kisit.kneu.edu.ua>
2023-02-02 07:36:50 +0200

    ehcache springboot-cache

--- src/main/java/ua/com/shop/course_shop_np_371_372_2023/config/AppConfiguration.java
+++ src/main/java/ua/com/shop/course_shop_np_371_372_2023/config/AppConfiguration.java
@@ -1,5 +1,5 @@
 package ua.com.shop.course_shop_np_371_372_2023.config;
-
+/*
 import org.ehcache.config.CacheConfiguration;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
@@ -14,26 +14,27 @@ import javax.cache.CacheManager;
 import javax.cache.Caching;
 import javax.cache.spi.CachingProvider;
 import java.time.Duration;
-@Configuration
+ */
+//@Configuration
 public class AppConfiguration {
 
-    @Bean
-    public CacheManager EhcacheManager() {
-
-        CacheConfiguration<String, Product> cacheConfig = CacheConfigurationBuilder
-                .newCacheConfigurationBuilder(String.class,
-                        Product.class,
-                        ResourcePoolsBuilder.newResourcePoolsBuilder()
-                                .offheap(10, MemoryUnit.MB)
-                                .build())
-                .withExpiry(ExpiryPolicyBuilder.timeToIdleExpiration(Duration.ofSeconds(10)))
-                .build();
-
-        CachingProvider cachingProvider = Caching.getCachingProvider();
-        CacheManager cacheManager = cachingProvider.getCacheManager();
-
-        var configuration = Eh107Configuration.fromEhcacheCacheConfiguration(cacheConfig);
-        cacheManager.createCache("prod", configuration);
-        return cacheManager;
-    }
+//    @Bean
+//    public CacheManager EhcacheManager() {
+//
+//        CacheConfiguration<String, Product> cacheConfig = CacheConfigurationBuilder
+//                .newCacheConfigurationBuilder(String.class,
+//                        Product.class,
+//                        ResourcePoolsBuilder.newResourcePoolsBuilder()
+//                                .offheap(10, MemoryUnit.MB)
+//                                .build())
+//                .withExpiry(ExpiryPolicyBuilder.timeToIdleExpiration(Duration.ofSeconds(10)))
+//                .build();
+//
+//        CachingProvider cachingProvider = Caching.getCachingProvider();
+//        CacheManager cacheManager = cachingProvider.getCacheManager();
+//
+//        var configuration = Eh107Configuration.fromEhcacheCacheConfiguration(cacheConfig);
+//        cacheManager.createCache("prod", configuration);
+//        return cacheManager;
+//    }
 }
